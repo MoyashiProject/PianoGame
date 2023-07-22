817a683bf0a86e415cc9e65ebed6fafda463c7c9
Yudai <105198285+masarusyogouki@users.noreply.github.com>
2023-07-22 15:15:05 +0900

    jetpack like todo

--- AndroidPianoProject/app/src/main/java/com/example/androidpianoproject/MainActivity.kt
+++ AndroidPianoProject/app/src/main/java/com/example/androidpianoproject/MainActivity.kt
@@ -3,13 +3,34 @@ package com.example.androidpianoproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.androidpianoproject.ui.theme.AndroidPianoProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +43,53 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val practiceList = (1..100).map {"練習曲${it}"}
+                    MainScreen()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidPianoProjectTheme {
-        Greeting("Android")
+fun MainScreen(){
+    var practiceList: SnapshotStateList<String> by remember { mutableStateOf(mutableStateListOf())}
+    var text: String by remember { mutableStateOf("") }
+    Column{
+        TopAppBar(
+            title = { Text("練習曲一覧")}
+        )
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth().weight(1f)
+        ) {
+            items(practiceList) { practice ->
+                Text(
+                    text = practice,
+                    modifier = Modifier.fillMaxWidth().padding(16.dp)
+                )
+            }
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(16.dp)
+        ) {
+            OutlinedTextField(
+                value = "",
+                onValueChange = { it -> text = it },
+                label = { Text("Practice")},
+                modifier = Modifier.wrapContentHeight().weight(1f)
+            )
+            Spacer(Modifier.size(16.dp))
+            Button(
+                onClick = {
+                    if (text.isEmpty()) return@Button
+                    practiceList.add(text)
+                    text = ""
+                },
+                modifier = Modifier.align(Alignment.CenterVertically)
+            ) {
+                Text("ADD")
+            }
+        }
     }
 }

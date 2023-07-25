11eda8a7b29fb2f7f4acb3e7a8c8ad5d6c69ac59
Takuchan <takuchanapp@gmail.com>
2023-07-25 14:01:32 +0900

    配列データに保存できるようになった

--- GeneratePiano/app/src/main/java/com/moyashi/generatepiano/AppDatabase.kt
+++ GeneratePiano/app/src/main/java/com/moyashi/generatepiano/AppDatabase.kt
@@ -5,8 +5,8 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 //practices格納
-@Database(entities = [Practice::class], version = 1, exportSchema = false) //それぞれ指定 -> エンティティ、バージョン、スキーマをエキスポートするかどうか
-@TypeConverters(DateTimeConverter::class)
+@Database(entities = [Practice::class], version = 2, exportSchema = false) //それぞれ指定 -> エンティティ、バージョン、スキーマをエキスポートするかどうか
+@TypeConverters(DateTimeConverter::class,StringListTypeConverter::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun practiceDao(): PracticeDao //practicesへのアクセス
 }

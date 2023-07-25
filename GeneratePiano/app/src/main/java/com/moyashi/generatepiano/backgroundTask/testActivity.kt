085cf831370ac805f3bff120e4df1c2575fee3d7
Takuchan <takuchanapp@gmail.com>
2023-07-25 09:09:30 +0900

    :+1:音の判別ができるようになった

--- GeneratePiano/app/src/main/java/com/moyashi/generatepiano/backgroundTask/testActivity.kt
+++ GeneratePiano/app/src/main/java/com/moyashi/generatepiano/backgroundTask/testActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -17,9 +19,13 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.moyashi.generatepiano.DetectSoundViewModel
@@ -52,16 +58,22 @@ class testActivity : ComponentActivity() {
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ) {
-                Column() {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
                     Button(onClick = {
                         val generated = GenerateScore().GenerateEasy()
                         text = generated.toString()
                     }) {
-                        counter.value?.let { count ->
-                            Text("$count")
-                        }
+                        Text("楽譜の生成")
                     }
                     Text(text=text)
+                    counter.value?.let { count ->
+
+                        Text("$count", textAlign = TextAlign.Center,modifier = Modifier.width(150.dp), fontSize = 50.sp)
+                    }
                 }
             }
         }

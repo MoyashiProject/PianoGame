e605fbdfcc90baba237cb24351ed3ee07fa80138
Takuchan <takuchanapp@gmail.com>
2023-07-27 21:48:46 +0900

    音符場所のやり直し

--- GeneratePiano/app/src/main/java/com/moyashi/generatepiano/MusicSheetScreen.kt
+++ GeneratePiano/app/src/main/java/com/moyashi/generatepiano/MusicSheetScreen.kt
@@ -29,7 +29,9 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
@@ -46,12 +48,16 @@ import java.util.Date
 fun MusicSheetScreen(practice:Practice?,viewModel:DetectSoundViewModel) {
 
     val counter = viewModel.counter.observeAsState()
-
+    val onpuHeight = viewModel.onpu_height.observeAsState()
     LockScreenOrientation(orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
     Box(
-        modifier = Modifier.fillMaxSize().background(Color.White)
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
     ) {
-        Column(modifier = Modifier.fillMaxSize().horizontalScroll(rememberScrollState())
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .horizontalScroll(rememberScrollState())
         ){
             Box(){
                 Column{
@@ -65,26 +71,7 @@ fun MusicSheetScreen(practice:Practice?,viewModel:DetectSoundViewModel) {
                         Box(
                             contentAlignment = Alignment.Center
                         ){
-                            Box{
-                                Canvas(
-                                    modifier = Modifier
-                                        .width(((practice?.right_hand?.size)?.times(45))!!.dp)
-                                        .height(500.dp)
-                                        .padding(vertical = 32.dp)
-                                        .background(Color.White)
-                                ) {
-                                    for (i in 0 .. 4) {
-                                        val y = ( i + 1 ) * size.height / 6 // 6等分した位置に線を描画する
-                                        drawLine(
-                                            color = Color.Black,
-                                            start = Offset(0f, y),
-                                            end = Offset(size.width, y),
-                                            strokeWidth = 5f
-                                        )
-                                    }
-                                }
-
-                            }
+                            gosenhu(practice)
                         }
 
                     }
@@ -98,39 +85,18 @@ fun MusicSheetScreen(practice:Practice?,viewModel:DetectSoundViewModel) {
                         Box(
                             contentAlignment = Alignment.Center
                         ){
-
-                            Box{
-                                Canvas(
-                                    modifier = Modifier
-                                        .width(((practice?.right_hand?.size)?.times(45))!!.dp)
-                                        .height(500.dp)
-                                        .padding(vertical = 32.dp)
-                                        .background(Color.White)
-                                ) {
-                                    for (i in 0 .. 4) {
-                                        val y = ( i + 1 ) * size.height / 6 // 6等分した位置に線を描画する
-                                        drawLine(
-                                            color = Color.Black,
-                                            start = Offset(0f, y),
-                                            end = Offset(size.width, y),
-                                            strokeWidth = 5f
-                                        )
-                                    }
-                                }
-
-                            }
-
-
+                            gosenhu(practice = practice)
                         }
 
                     }
                 }
                 Row(modifier = Modifier.padding(start = 120.dp)){
                     practice?.right_hand?.forEach{ hand ->
-                        Box(modifier = Modifier.padding(start = 44.dp).offset(y = (-90).dp)){
-                            setOnpu(name = "${hand}")
+                        Box(modifier = Modifier
+                            .padding(start = 44.dp)
+                            .offset(y = (-93).dp)) {
+                            setOnpu(name = "$hand")
                         }
-                        Text(text = "${hand}")
                     }
                 }
             }
@@ -138,68 +104,91 @@ fun MusicSheetScreen(practice:Practice?,viewModel:DetectSoundViewModel) {
 
         }
 
+        clef(drawable = "clef")
+        clef(drawable = "bass_clef")
 
-        // Draw the line on the Canvas
-
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.CenterStart,
-        ) {
+//        counter.value?.let { count ->
+//            Text("$count", textAlign = TextAlign.Center,modifier = Modifier.width(150.dp), fontSize = 50.sp)
+//        }
+    }
+}
+@Composable
+fun clef(drawable: String){
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.CenterStart
+    ){
+        if(drawable == "clef"){
             Image(
                 painter = painterResource(id = R.drawable.clef),
                 contentDescription = null,
                 modifier = Modifier
                     .height(200.dp)
                     .width(100.dp)
-                    .offset(x = (-20).dp,y = (-90).dp)
+                    .offset(x = (-20).dp, y = (-90).dp)
             )
             ViewFraction(upText = 4, downText = 4, x = 80,y1= -115,y2 = -73)
-        }
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.CenterStart,
-        ) {
+        }else if(drawable == "bass_clef"){
             Image(
                 painter = painterResource(id = R.drawable.bass_clef),
                 contentDescription = null,
                 modifier = Modifier
                     .height(80.dp)
                     .width(80.dp)
-                    .offset(x = (-0).dp,y = (90).dp)
+                    .offset(x = (-0).dp, y = (90).dp)
             )
             ViewFraction(upText = 4, downText = 4, x = 80,y1= 110,y2 = 68)
         }
-        counter.value?.let { count ->
-            Text("$count", textAlign = TextAlign.Center,modifier = Modifier.width(150.dp), fontSize = 50.sp)
-        }
     }
 
+
+}
+@Composable
+fun gosenhu(practice: Practice?){
+    Box{
+        Canvas(
+            modifier = Modifier
+                .width(((practice?.right_hand?.size)?.times(5))!!.dp)
+                .fillMaxHeight()
+                .padding(vertical = 32.dp)
+                .background(Color.White)
+
+        ) {
+            for (i in 0 .. 4) {
+                val y = ( i + 1 ) * size.height / 6 // 6等分した位置に線を描画する
+                drawLine(
+                    color = Color.Black,
+                    start = Offset(0f, y),
+                    end = Offset(size.width, y),
+                    strokeWidth = 5f
+                )
+            }
+
+        }
+
+    }
 }
 @Composable
 fun setOnpu(name: String){
     for (pre in enum_musicStep.values()){
         if(pre.names == name){
-            Box(modifier = Modifier.offset(y = (-6.5).times(pre.id-51).dp)){
+            if(pre.id > 51){
+                Text("${pre.jpName}")
+                //音階が真ん中シ（シ３)よりも上だった場合のプログラム
+                Box(modifier = Modifier.offset(y = (-6.5).times(pre.id-51).dp)){
                     ShibuOnpu()
 
                 }
-//            if(pre.id > 51){
-//                Text("${pre.jpName}")
-//                //音階が真ん中シ（シ３)よりも上だった場合のプログラム
-//                Box(modifier = Modifier.offset(y = (-6.5).times(pre.id-51).dp)){
-//                    ShibuOnpu()
-//
-//                }
-//            }else{
-//                if(pre.id > 38){
-//                    Box(modifier = Modifier.offset(y = (6.5).times(51-pre.id).dp)){
-//                        ShibuOnpu()
-//                    }
-//                }else{
-//
-//                }
-//
-//            }
+            }else{
+                if(pre.id > 38){
+                    Box(modifier = Modifier.offset(y = (6.5).times(51-pre.id).dp)){
+                        ShibuOnpu()
+                    }
+                }else{
+
+                }
+
+            }
         }
     }
 }
--- GeneratePiano/app/src/main/java/com/moyashi/generatepiano/function/Onpu.kt
+++ GeneratePiano/app/src/main/java/com/moyashi/generatepiano/function/Onpu.kt
@@ -1,43 +1,67 @@
 package com.moyashi.generatepiano.function
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import kotlin.math.cos
 import kotlin.math.sin
 
 @Composable
 fun ShibuOnpu(){
-    Canvas(modifier = Modifier.fillMaxSize()) {
-        // Draw the ellipse (similar to the previous code)
-        rotate(degrees = -30F) {
-            scale(scaleX = 0.65f, scaleY = 6.4f.dp.toPx() / 22.1.dp.toPx()) {
-                drawCircle(Color.Black, radius = 22.1.dp.toPx())
-            }
+
+    val ellipseWidth = 22.5.dp
+    val ellipseHeight = 14.dp
+    val lineWidth = 2.dp
+    val lineLength = 50.dp
+    Canvas(
+        modifier = Modifier
+            .size(100.dp)
+    ) {
+        // 楕円を描画する
+        rotate(degrees = (-20F)){
+            drawOval(
+                color = Color.Black,
+                topLeft = Offset(
+                    x = (size.width - ellipseWidth.toPx()) / 2,
+                    y = (size.height - ellipseHeight.toPx()) / 2
+                ),
+                size = Size(ellipseWidth.toPx(), ellipseHeight.toPx())
+            )
         }
 
-        // Calculate the starting and ending points for the vertical line
-        val centerX = size.width / 2
-        val centerY = size.height / 2
-        val lineStart = Offset(centerX + 10.dp.toPx(), centerY - 70.dp.toPx())
-        val lineEnd = Offset(centerX + 10.dp.toPx(), centerY)
-
-        // Draw the vertical line
+        // 黒い線を描画する
         drawLine(
             color = Color.Black,
-            start = lineStart,
-            end = lineEnd,
-            strokeWidth = 3.dp.toPx()
+            start = Offset(
+                x = (size.width + ellipseWidth.toPx()) / 2,
+                y = (size.height - ellipseHeight.toPx()) / 2
+            ),
+            end = Offset(
+                x = (size.width + ellipseWidth.toPx()) / 2,
+                y = (size.height - ellipseHeight.toPx()) / 2 - lineLength.toPx()
+            ),
+            strokeWidth = lineWidth.toPx()
         )
     }
+}
+@Composable
+internal fun Float.pxToDp(): Dp {
+    return (this / LocalDensity.current.density).dp
 }
 @Preview
 @Composable

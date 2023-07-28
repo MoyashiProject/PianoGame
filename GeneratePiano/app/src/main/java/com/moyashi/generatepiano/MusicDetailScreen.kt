99100cca1ab7f187758577046072ce3b93781066
Takuchan <takuchanapp@gmail.com>
2023-07-28 14:35:13 +0900

    みんなの編集を上げる

--- GeneratePiano/app/src/main/java/com/moyashi/generatepiano/MusicDetailScreen.kt
+++ GeneratePiano/app/src/main/java/com/moyashi/generatepiano/MusicDetailScreen.kt
@@ -1,16 +1,24 @@
 package com.moyashi.generatepiano
 
-
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,10 +26,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-
 @Composable
 fun MusicDetailScreen(practice: Practice?, navController: NavHostController, viewModel: PracticeViewModel) {
     //MainScrennでクリックされたPracticeItemの詳細を表示
@@ -31,38 +40,57 @@ fun MusicDetailScreen(practice: Practice?, navController: NavHostController, vie
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
+            .clickable {}
     ) {
+        MusicNoteBackground()
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            IconButton(
+                onClick = { navController.popBackStack(MainActivity.Route.FIRST.name,
+                    inclusive = false)
+                },
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "戻る")
+            }
+            IconButton(
+                onClick = {
+                    selectedPractice?.let { practiceToDelete ->
+                        viewModel.deletePractice(practice = practiceToDelete)
+                        navController.navigate((MainActivity.Route.FIRST.name))
+                    }
+                },
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Delete,
+                    contentDescription = "削除"
+                )
+            }
+        }
         Column(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
+                .padding(16.dp)
                 .verticalScroll(rememberScrollState())
-                .align(Alignment.Center)
         ) {
             if (practice != null) {
                 Text(
                     text = practice.title,
                     style = typography.h5.copy(fontWeight = FontWeight.Bold),
-                    modifier = Modifier.padding(bottom = 8.dp)
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(bottom = 8.dp)
                 )
                 Text(
                     text = "作成日時 : ${practice.created_at}",
                     style = typography.body1,
                     modifier = Modifier.padding(bottom = 8.dp)
                 )
-                if(practice.difficult){
-                    Text(
-                        text = "ハードモード",
-                        style = typography.body1,
-                        modifier = Modifier.padding(bottom = 8.dp)
-                    )
-                }else{
-                    Text(
-                        text = "イージーモード",
-                        style = typography.body1,
-                        modifier = Modifier.padding(bottom = 8.dp)
-                    )
-                }
-
                 Text(
                     text="右手:${practice.right_hand}",
                     style = typography.body1,
@@ -76,34 +104,34 @@ fun MusicDetailScreen(practice: Practice?, navController: NavHostController, vie
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .align(Alignment.BottomCenter)
+                .padding(16.dp)
+                .align(Alignment.BottomEnd)
         ) {
             Button(
                 onClick = {
-                    navController.popBackStack(MainActivity.Route.FIRST.name, inclusive = false)
-                },
-                modifier = Modifier.padding(end = 8.dp)
-            ) {
-                Text("戻る")
-            }
-            Button(
-                onClick = {
-//                    navController.navigate(MainActivity.Route.SECOND.name)
-                     navController.navigate("${MainActivity.Route.SECOND.name}/${practice?.id}")
+                    navController.navigate(MainActivity.Route.SECOND.name)
                 }
             ) {
                 Text("練習する")
             }
-            Button(
-                onClick = {
-                    selectedPractice?.let { practiceToDelete ->
-                        viewModel.deletePractice(practice = practiceToDelete)
-                        navController.navigate((MainActivity.Route.FIRST.name))
-                    }
-                }
-            ) {
-                Text("削除する")
-            }
         }
+    }
+}
+@Composable
+fun MusicNoteBackground() {
+    // 八分音符のテキストスタイルを定義
+    val noteStyle = androidx.compose.ui.text.TextStyle(
+        fontSize = 200.sp,
+        color = Color.Gray
+    )
+    // Box コンポーネントで背景と他の要素を重ねる
+    Box(modifier = Modifier.fillMaxSize()) {
+        // 背景に八分音符のテキストを配置
+        Text(
+            text = "\u266B",
+            style = noteStyle,
+            modifier = Modifier.align(Alignment.Center)
+        )
+
     }
 }
